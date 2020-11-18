510582eaff2d4b95c5aadccf0ccc736b5d4b50c5
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2020-11-18 10:56:15 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,7 +6,7 @@ echo "$API_ClientID" > /verbs/API_ClientID
 echo "$API_ClientSecret" > /verbs/API_ClientSecret
 echo "$API_GrantType" >  /verbs/API_GrantType
 echo "$API_RedirectUri" > /verbs/API_RedirectUri
-echo "api_server_prefix" > /verbs/api_server_prefix
+echo "$api_server_prefix" > /verbs/api_server_prefix
 
 service cron start
 tail -f log.log
